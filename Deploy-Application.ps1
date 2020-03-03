7167ebcc4f162a46bdc02d7ee0bbe17f69e5427b
truongnguyen-msudenver <tnguy189@msudenver.edu>
2020-03-02 22:53:28 -0700

    Update Deploy-Application.ps1
    Ignore exit code 1223

--- Deploy-Application.ps1
+++ Deploy-Application.ps1
@@ -139,8 +139,9 @@ Try {
 		}
 
 		## <Perform Installation tasks here>
-		$exitCode = Execute-Process -Path "$dirFiles\Mendeley-Desktop-1.19.4-win32.exe" -Parameters '/S' -WindowStyle 'Hidden'
-		If (($exitCode.ExitCode -ne "0") -and ($exitCode.ExitCode -ne "1223") -and ($mainExitCode -ne "3010")) { $mainExitCode = $exitCode.ExitCode }
+		$exitCode = Execute-Process -Path "$dirFiles\Mendeley-Desktop-1.19.4-win32.exe" -Parameters '/S' -WindowStyle 'Hidden' -IgnoreExitCodes '1223' -PassThru
+		If ($exitCode.ExitCode -ne "1223") {$exitCode.ExitCode = "0"}
+		If (($exitCode.ExitCode -ne "0") -and ($mainExitCode -ne "3010")) { $mainExitCode = $exitCode.ExitCode }
 
 
 		##*===============================================
